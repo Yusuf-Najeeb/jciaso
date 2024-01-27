c5a5ee474887ff4085749ed4e7fa9ec16ce7b18c
Muhammad Najeeb <yusnaj21@gmail.com>
2024-01-27 23:51:24 +0100

    Initialized: articles and social media posts sections

--- src/components/homepage/LandingPage.jsx
+++ src/components/homepage/LandingPage.jsx
@@ -3,12 +3,14 @@ import HeroSection from "./herosection/HeroSection";
 import Vision from "./jcivision/Vision";
 import Sponsors from "./sponsors/Sponsors";
 import Footer from "../footer/Footer";
+import Articles from "./articles/Articles";
 
 const LandingPage = () => {
   return (
     <React.Fragment>
       <HeroSection />
       <Vision />
+      <Articles />
       <Sponsors />
       <Footer />
     </React.Fragment>
