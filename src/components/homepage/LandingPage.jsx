153bdc3e50a444fb9267671eaa69d5040eba43e2
eedrisofficial <eedrisofficial.gmail.com>
2024-01-27 21:32:33 -0600

    updates on article

--- src/components/homepage/LandingPage.jsx
+++ src/components/homepage/LandingPage.jsx
@@ -3,12 +3,14 @@ import HeroSection from "./herosection/HeroSection";
 import Vision from "./jcivision/Vision";
 import Sponsors from "./sponsors/Sponsors";
 import Footer from "../footer/Footer";
+import Testimonials from "./testimonials/Testimonials";
 
 const LandingPage = () => {
   return (
     <React.Fragment>
       <HeroSection />
       <Vision />
+      <Testimonials />
       <Sponsors />
       <Footer />
     </React.Fragment>
---
+++ src/localdb/Article.js
@@ -0,0 +1,35 @@
+export const article = [
+  {
+    Image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1706383292713/952dbe19-8e67-4773-b9fb-f60b1d862dd4.png",
+    post1: "POLITICS",
+    post2: "CONSTITUTION",
+    heading: "2023 Constituency and Manual",
+    date: "11th June 2023",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.",
+    read: "Read More",
+  },
+  {
+    Image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1706383292713/952dbe19-8e67-4773-b9fb-f60b1d862dd4.png",
+    post1: "POLITICS",
+    post2: "CONSTITUTION",
+    heading: "2023 Constituency and Manual",
+    date: "11th June 2023",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.",
+    read: "Read More",
+  },
+  {
+    Image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1706383292713/952dbe19-8e67-4773-b9fb-f60b1d862dd4.png",
+    post1: "POLITICS",
+    post2: "CONSTITUTION",
+    heading: "2023 Constituency and Manual",
+    date: "11th June 2023",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.",
+    read: "Read More",
+  },
+];
