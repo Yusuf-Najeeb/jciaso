73849eedfdaa6c8dc22a7f36f45e718495173b5e
eedrisofficial <eedrisofficial.gmail.com>
2024-01-27 21:44:47 -0600

    update

--- src/components/homepage/LandingPage.jsx
+++ src/components/homepage/LandingPage.jsx
@@ -5,10 +5,12 @@ import Sponsors from "./sponsors/Sponsors";
 import Footer from "../footer/Footer";
 import Testimonials from "./testimonials/Testimonials";
 import Articles from "./articles/Articles";
+import Header from "../header/Header";
 
 const LandingPage = () => {
   return (
     <React.Fragment>
+      <Header />
       <HeroSection />
       <Vision />
       <Articles />
