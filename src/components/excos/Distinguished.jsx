a384009e89bf740aa82b7e336c505800a679cbbb
Oyedunmade1 <doyedunmade@gmail.com>
2024-01-31 13:35:48 +0100

    new commits

---
+++ src/components/excos/Distinguished.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { jciDistinguish } from "../../localdb/Distinguish";
+import MemberFrame from "./MemberFrame";
+
+const Distinguished = () => {
+  const [exco] = useState(jciDistinguish);
+  return (
+    <div className="flex flex-col gap-12 justify-center items-center py-24 px-6 lg:px-16">
+      <div className="flex flex-col items-center">
+        <h1 className="title">Meet the Executive Members</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
+          mi.
+        </p>
+      </div>
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1  justify-center items-center gap-8 ">
+        {exco.map((member) => (
+          <MemberFrame member={member} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Distinguished;
