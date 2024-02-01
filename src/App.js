141dc7ca2545d3fcfd9babddb8b71cf8e8f7174c
Muhammad Najeeb <yusnaj21@gmail.com>
2024-02-01 16:03:36 +0100

    Initialized: Manula Page

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import "./index.css";
 import ContactUs from "./components/contactpage/ContactUs";
 import About from "./components/about-page/About";
 import Excos from "./components/excos/Excos";
-import Article from "./components/article-page/Article";
+import Manual from "./components/constitution/Manual";
 
 function App() {
   return (
-    // <div className="bg-[#FFFFFF] text-[#1F1D21] ">
-    //   <Routes>
-    //     <Route path="/" element={<LandingPage />} />
-    //     <Route path="/contact" element={<ContactUs />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/excos" element={<Excos />} />
-    //   </Routes>
-    // </div>
-    <div>
-      <Article />
+    <div className="bg-[#FFFFFF] text-[#1F1D21] ">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/excos" element={<Excos />} />
+      </Routes>
     </div>
   );
 }
--- src/components/article-page/ArticleHero.jsx
+++ src/components/article-page/ArticleHero.jsx
@@ -5,8 +5,8 @@ const ArticleHero = () => {
     <section className="">
       <div className="h-[50vh] bg-[url('https://cdn.hashnode.com/res/hashnode/image/upload/v1706792495891/04441555-4fe1-4d00-9dfb-d8b81b787fca.jpeg')] bg-cover bg-bottom w-full">
         <div className="bg-[#2288A952] text-white backdrop-blur-0 h-[50vh] flex flex-col justify-center items-start px-8">
-          <h1 className="text-5xl font-bold">News & Articles</h1>
-          <p className="w-[30%] my-2">
+          <h1 className="text-6xl font-bold">News & Articles</h1>
+          <p className="w-[30%] my-2 text-[1.2rem]">
             Meet our able management and the people who make our Institution
             feel like home.
           </p>
