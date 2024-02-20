1edde73fd7370877c9ddabc500d770de7453b0ad
Muhammad Najeeb <yusnaj21@gmail.com>
2024-02-20 01:32:42 +0100

    Modified: Landing page's responsiveness and content down to vision section

--- src/components/homepage/jcivision/Vision.jsx
+++ src/components/homepage/jcivision/Vision.jsx
@@ -16,8 +16,8 @@ const Vision = () => {
   const now = new Date();
   return (
     <React.Fragment>
-      <div className="flex flex-col gap-16 w-[100%] px-6 lg:flex-row lg:px-16 lg:py-10 lg:mt-16">
-        <section className="flex flex-col items-center gap-6 lg:w-[50%]">
+      <div className="flex flex-col gap-16 w-[100%] pb-2 lg:flex-row lg:px-16 lg:py-10 lg:mt-16">
+        <section className="flex flex-col items-center gap-4 lg:w-[50%] border">
           <div
             data-aos="fade-up"
             data-aos-duration="800"
@@ -27,13 +27,20 @@ const Vision = () => {
             <img
               src="https://cdn.hashnode.com/res/hashnode/image/upload/v1706434169210/6a30b8fa-0719-4c52-b001-9e1490aa2f08.png"
               alt="JCI_President"
-              className="w-full rounded-lg object-cover"
+              className="w-full rounded-lg object-cover relative z-10"
             />
             <div className="flex flex-col items-center">
-              <h1 className="text-[1.5rem] font-bold">
+              <h1
+                className="relative sm:text-[1.5rem] font-bold sm:font-semibold pb-2 pl-2 border-l-2 border-b-2 mt-4 mb-1"
+                style={{
+                  borderBottomColor: "#EC4B14",
+                  borderLeftColor: "#333",
+                }}
+              >
+                <div className="absolute top-0 left-0 -translate-x-[7px] w-3 h-3 rounded-full bg-[#EC4B14]"></div>
                 JCIN Amb. Yetunde Adebisi
               </h1>
-              <p className="font-semibold">
+              <p className="text-[1rem] sm:text-2xl font-semibold">
                 24<sup>th</sup> President JCI Aso
               </p>
             </div>
@@ -43,9 +50,9 @@ const Vision = () => {
             data-aos="fade-up"
             data-aos-duration="1000"
             data-aos-easing="linear"
-            className="flex flex-col items-start gap-2"
+            className="flex flex-col items-start gap-2 p-2"
           >
-            <h1 className="font-bold text-[1.8rem] lg:text-[2.5rem] text-center">
+            <h1 className="font-semibold text-center sm:text-left sm:text-[1.5rem] lg:text-[1.8rem]">
               {now.getFullYear()} New Year Message
             </h1>
             <p className="text-start">
@@ -53,7 +60,7 @@ const Vision = () => {
               fostering a vibrant and inclusive community. By putting our
               members first, cultivating partnerships, and prioritizing
               sustainability, Together, we will create lasting impact and
-              inspire positive change in our region and beyond.
+              inspire positive change in our region and beyond...
             </p>
             <Link to="">
               <button className="border border-[#2288A9] text-[#2288A9] font-medium px-4 py-2 rounded-lg hover:border-red-500 hover:text-red-500 duration-300 mb-4">
@@ -67,11 +74,11 @@ const Vision = () => {
             data-aos="flip-right"
             data-aos-duration="400"
             data-aos-easing="ease-in-out"
-            className="font-bold title text-[1.8rem] lg:text-[2.5rem] text-center"
+            className="font-semibold title text-[1.8rem] lg:text-[2.5rem] text-center"
           >
-            2024 Presidential theme
+            Core Objectives
           </h1>
-          <img className="w-[80%]" src={Theme} alt="" />
+          <img className="w-[60%]" src={Theme} alt="" />
 
           <section className="flex flex-col gap-16 ">
             <div
@@ -84,7 +91,7 @@ const Vision = () => {
                 <MdCenterFocusStrong size={30} color="#FFF" />
               </p>
               <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-[1.2rem] ">
+                <h3 className="font-semibold text-[1.2rem] ">
                   Member-Centric Approach
                 </h3>
                 <p className="font-medium text-slate-500">
@@ -94,6 +101,7 @@ const Vision = () => {
                 </p>
               </div>
             </div>
+
             <div
               data-aos="fade-up"
               data-aos-duration="400"
@@ -101,28 +109,10 @@ const Vision = () => {
               className="flex flex-col items-center text-center gap-6 md:text-start md:flex-row  "
             >
               <p className="bg-[#20B4E3] p-10 rounded-full ">
-                <FaProjectDiagram size={30} color="#FFF" />
-              </p>
-              <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-[1.2rem] ">Engaging Projects</h3>
-                <p className="font-medium text-slate-500">
-                  Organize health walks to promote physical well-being and
-                  community bonding, and establish a mentorship program to
-                  support personal and professional growth.
-                </p>
-              </div>
-            </div>
-            <div
-              data-aos="fade-up"
-              data-aos-duration="500"
-              data-aos-easing="ease-in-out"
-              className="flex flex-col items-center text-center gap-6 md:text-start md:flex-row  "
-            >
-              <p className="bg-[#20B4E3] p-10 rounded-full ">
                 <GiWeightLiftingUp size={30} color="#FFF" />
               </p>
               <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-[1.2rem]">
+                <h3 className="font-semibold text-[1.2rem]">
                   Strengthening Local Collaborations
                 </h3>
                 <p className="font-medium text-slate-500">
@@ -133,6 +123,49 @@ const Vision = () => {
                 </p>
               </div>
             </div>
+
+            <div
+              data-aos="fade-up"
+              data-aos-duration="500"
+              data-aos-easing="ease-in-out"
+              className="flex flex-col items-center text-center gap-6 md:text-start md:flex-row  "
+            >
+              <p className="bg-[#20B4E3] p-10 rounded-full ">
+                <MdOutline6FtApart size={30} color="#FFF" />
+              </p>
+              <div className="flex flex-col gap-2">
+                <h3 className="font-semibold text-[1.2rem]">Partnership</h3>
+                <p className="font-medium text-slate-500">
+                  Conducting regular financial audits ensures transparency,
+                  while also facilitating knowledge sharing and capacity
+                  building initiatives. Enhancing brand awareness and visibility
+                  is achieved through various channels, including the
+                  publication of partners.
+                </p>
+              </div>
+            </div>
+
+            {/* <div
+              data-aos="fade-up"
+              data-aos-duration="500"
+              data-aos-easing="ease-in-out"
+              className="flex flex-col items-center text-center gap-6 md:text-start md:flex-row  "
+            >
+              <p className="bg-[#20B4E3] p-10 rounded-full ">
+                <FaProjectDiagram size={30} color="#FFF" />
+              </p>
+              <div className="flex flex-col gap-2">
+                <h3 className="font-semibold text-[1.2rem] ">
+                  Engaging Projects
+                </h3>
+                <p className="font-medium text-slate-500">
+                  Organize health walks to promote physical well-being and
+                  community bonding, and establish a mentorship program to
+                  support personal and professional growth.
+                </p>
+              </div>
+            </div>
+
             <div
               data-aos="fade-up"
               data-aos-duration="600"
@@ -143,33 +176,15 @@ const Vision = () => {
                 <FaGlobe size={30} color="#FFF" />
               </p>
               <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-[1.2rem]">Global Engagement</h3>
+                <h3 className="font-semibold text-[1.2rem]">
+                  Global Engagement
+                </h3>
                 <p className="font-medium text-slate-500">
                   Promote international partnerships to exchange ideas and
                   implement cross-border initiatives.
                 </p>
               </div>
-            </div>
-            <div
-              data-aos="fade-up"
-              data-aos-duration="700"
-              data-aos-easing="ease-in-out"
-              className="flex flex-col items-center text-center gap-6 md:text-start md:flex-row  "
-            >
-              <p className="bg-[#20B4E3] p-10 rounded-full ">
-                <MdOutline6FtApart size={30} color="#FFF" />
-              </p>
-              <div className="flex flex-col gap-2">
-                <h3 className="font-bold text-[1.2rem]">Partnership</h3>
-                <p className="font-medium text-slate-500">
-                  Conducting regular financial audits ensures transparency,
-                  while also facilitating knowledge sharing and capacity
-                  building initiatives. Enhancing brand awareness and visibility
-                  is achieved through various channels, including the
-                  publication of partners.
-                </p>
-              </div>
-            </div>
+            </div> */}
           </section>
         </section>
       </div>
