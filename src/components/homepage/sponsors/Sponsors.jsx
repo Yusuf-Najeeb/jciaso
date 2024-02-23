5b0c0911cd1a4beb2bd02463fcc138bb4af01db0
Muhammad Najeeb <yusnaj21@gmail.com>
2024-02-23 22:47:18 +0100

    Modified: Sponsors section

--- src/components/homepage/sponsors/Sponsors.jsx
+++ src/components/homepage/sponsors/Sponsors.jsx
@@ -6,45 +6,56 @@ const Sponsors = () => {
       <h1 className=" title font-bold text-center text-[1.8rem] lg:text-[2.5rem]">
         Our Partners
       </h1>
-      <section className="flex flex-wrap justify-center items-start gap-6 lg:gap-16">
-        <img
-          src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450578/IMG-20240128-WA0032_oztynh.jpg"
-          alt="SEC"
-          className="w-[20rem] h-[6rem]  object-contain"
-        />
-        <img
-          src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450579/IMG-20240128-WA0034_ciq8jr.jpg"
-          alt="SamVic"
-          className="w-[20rem] h-[6rem]  object-contain"
-        />
-        <img
-          src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450579/IMG-20240128-WA0030_jjafub.jpg"
-          alt="Transcorp"
-          className="w-[20rem] h-[6rem]  object-contain"
-        />
-
-        <img
-          src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450580/IMG-20240128-WA0028_njsiwy.jpg"
-          alt="Boff"
-          className="w-[20rem] h-[6rem]  object-contain"
-        />
-        <img
-          src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450580/IMG-20240128-WA0031_d3wrlb.jpg"
-          alt="Virtusso"
-          className="w-[20rem] h-[6rem]  object-contain"
-        />
-
-        <img
-          src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450581/IMG-20240128-WA0033_usqfj8.jpg"
-          alt="Studio 24"
-          className="w-[20rem] h-[6rem]  object-contain"
-        />
-
-        <img
-          src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450584/IMG-20240128-WA0029_euimvo.jpg"
-          alt="TIIDELab"
-          className="w-[20rem] h-[6rem]  object-contain"
-        />
+      <section className="grid grid-cols-2 md:grid-cols-4 gap-4 place-content-center place-items-center justify-center items-center lg:gap-16">
+        <div className="bg-white shadow min-h-[200px] max-h-[250px]">
+          <img
+            src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450578/IMG-20240128-WA0032_oztynh.jpg"
+            alt="SEC"
+            className="w-full object-contain object-center"
+          />
+        </div>
+        <div className="bg-white shadow min-h-[200px] max-h-[250px]">
+          <img
+            src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450579/IMG-20240128-WA0034_ciq8jr.jpg"
+            alt="SamVic"
+            className="w-full object-contain object-center"
+          />
+        </div>
+        <div className="bg-white shadow min-h-[200px] max-h-[250px]">
+          <img
+            src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450579/IMG-20240128-WA0030_jjafub.jpg"
+            alt="Transcorp"
+            className="w-full object-contain object-center"
+          />
+        </div>
+        <div className="bg-white shadow min-h-[200px] max-h-[250px]">
+          <img
+            src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450580/IMG-20240128-WA0028_njsiwy.jpg"
+            alt="Boff"
+            className="w-full object-contain object-center"
+          />
+        </div>
+        <div className="bg-white shadow min-h-[200px] max-h-[250px]">
+          <img
+            src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450580/IMG-20240128-WA0031_d3wrlb.jpg"
+            alt="Virtusso"
+            className="w-full object-contain object-center"
+          />
+        </div>
+        <div className="bg-white shadow min-h-[200px] max-h-[250px]">
+          <img
+            src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450581/IMG-20240128-WA0033_usqfj8.jpg"
+            alt="Studio 24"
+            className="w-full object-contain object-center"
+          />
+        </div>
+        <div className="bg-white shadow min-h-[200px] max-h-[250px]">
+          <img
+            src="https://res.cloudinary.com/duw4jtxls/image/upload/v1706450584/IMG-20240128-WA0029_euimvo.jpg"
+            alt="TIIDELab"
+            className="w-full object-contain object-center"
+          />
+        </div>
       </section>
     </div>
   );
