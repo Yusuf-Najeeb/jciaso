b807034ac1a5a00961f055e7c5e08c6ba2f17c20
Oyedunmade1 <doyedunmade@gmail.com>
2024-03-13 14:31:59 +0100

    new

--- src/localdb/Distinguish.js
+++ src/localdb/Distinguish.js
@@ -16,12 +16,14 @@ export const jciDistinguish = [
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710316424857/7e24defe-6af6-4fed-a50b-590a03e56748.png",
+    sen: "JCI Sen.",
     name: "Adebayo Morakinyo",
     position: "President-2008",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710331552145/04130dd6-5625-46cc-a34f-3fb7c35c1928.png",
+    sen: "JCI Sen.",
     name: "Babajide Richard Ojo",
     position: "President-2011",
   },
@@ -29,60 +31,70 @@ export const jciDistinguish = [
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710316427633/dc8d57b1-6989-4cca-93c3-2293e36dea0e.png",
+    sen: "JCI Sen.",
     name: "Muhammadu Mujtaba K.",
     position: "President-2012",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710331554546/b589eb2a-b9c6-4f30-b066-0da7922eb1ff.png",
+    sen: "JCI Sen.",
     name: "Aina Segun Aina",
     position: "President-2013",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710316128948/43169746-84ef-4699-a61d-237fe1d87227.png",
+    sen: "JCI Sen.",
     name: "Odusole Oluwakayode",
     position: "President-2014",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710315746745/41b52ebc-5051-4b9a-b133-922c2b1e43b8.png",
+    sen: "JCI Sen.",
     name: "Oluwaseyi Orimoloye",
     position: "President-2016",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710315811189/56a85c68-7d8d-43be-82fa-7b7fe31fdb18.png",
+    sen: "JCI Sen.",
     name: "Oluwafemi Fadairo",
     position: "President-2018",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710315806154/da689556-4ed3-4e6c-ab35-d87783e9ad8d.png",
+    sen: "JCI Sen.",
     name: "Attairu Abdullahi B.",
     position: "President-2019",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710315751964/13681fb9-eb54-43e6-bcc2-38cabb9a928e.png",
+    sen: "JCI Sen.",
     name: "Ayodeji Ojo",
     position: "President-2020",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710316123653/59dd344d-b0fd-4543-bb38-babf29926c95.png",
+    sen: "JCI Sen.",
     name: "Ajala Aminat A.",
     position: "President-2021",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710315754334/c56e8f11-1502-432c-8f0a-e6c2142126f2.png",
+    sen: "JCI Sen.",
     name: "Abayomi Oladipupo L.",
     position: "President-2022",
   },
   {
     image:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1710315749823/6bd5d833-e8de-44ce-9d62-0df0dcdd8917.png",
+    sen: "JCI Sen.",
     name: "Olubunmi Ajayi",
     position: "President-2023",
   },
