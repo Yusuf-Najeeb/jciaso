9e3cf58ab47be3281065185a6e6c77e50159b205
Muhammad Najeeb <yusnaj21@gmail.com>
2024-06-18 23:55:41 +0100

    Modified: Typo correction

--- src/components/Error.js
+++ src/components/Error.js
@@ -10,7 +10,7 @@ const ErrorPage = () => {
         <p className="-mt-10 text-xl">404 Page Not Found!</p>
         <p className="text-xl my-4">
           The page you are looking for might have been removed, has its name
-          changes or temporarily unavailable.
+          changed or temporarily unavailable.
         </p>
         <button className="bg-[#EC4B14] text-white rounded-xl px-4 py-2 animate-bounce my-4">
           <Link to={"/"}>Go to Home</Link>
