8b4e0cbb3b3e46f339ebe35b5447dca2137a5409
Muhammad Najeeb <yusnaj21@gmail.com>
2024-09-14 23:39:16 +0100

    Initialized: About Event Section

---
+++ src/components/QLV/About.jsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <main className="bg-[#FCFCFC] text-[#001926] pt-[6rem] pb-8">
+      <h2 className="text-5xl font-bold text-center">About Event</h2>
+      <div className="flex flex-col md:flex-row gap-4">
+        <section>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+          molestiae, eaque, a dolorum est alias nulla assumenda facere error
+          aliquam consectetur neque architecto culpa. Animi obcaecati
+          voluptatibus possimus labore error.
+        </section>
+        <section>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+          molestiae, eaque, a dolorum est alias nulla assumenda facere error
+          aliquam consectetur neque architecto culpa. Animi obcaecati
+          voluptatibus possimus labore error.
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default About;
--- src/components/QLV/index.jsx
+++ src/components/QLV/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "./Header";
 import Hero from "./Hero";
 import CountDown from "./CountDown";
+import About from "./About";
 
 const index = () => {
   return (
@@ -9,6 +10,7 @@ const index = () => {
       <Nav />
       <Hero />
       <CountDown />
+      <About />
     </div>
   );
 };
