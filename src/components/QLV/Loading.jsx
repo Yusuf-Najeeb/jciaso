a5aa7867961c0e4f5574e7225622c19685822ce3
Muhammad Najeeb <yusnaj21@gmail.com>
2024-09-18 20:41:58 +0100

    Modified: QLV Loading page

--- src/components/QLV/Loading.jsx
+++ src/components/QLV/Loading.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import QLVLogo from "../QLV/assets/QLV1.png";
+import QLVLogo1 from "../QLV/assets/QLV2.png";
 
 const LandingPage = () => {
   const [progress, setProgress] = useState(0);
@@ -17,19 +18,19 @@ const LandingPage = () => {
     <div
       className="bg-gray-100 bg-cover bg-center flex justify-center items-center min-h-screen"
       style={{
-        backgroundImage: `url(${QLVLogo})`,
+        backgroundImage: `url(${QLVLogo1})`,
       }}
     >
-      <div className="w-full max-w-md p-6 rounded-lg mt-[24rem]">
-        <div className="w-full bg-gray-300 rounded-full h-4">
+      <div className="w-full max-w-md p-6 rounded-lg mt-[30rem]">
+        <div className="w-full bg-gray-300 h-4">
           <div
-            className="h-4 rounded-full striped progress-bar"
+            className="h-4 striped progress-bar"
             style={{ width: `${progress}%` }}
           ></div>
         </div>
 
-        <p className="text-center text-3xl font-bold mb-6 mt-4 text-[#333]">
-          QLV 24 is Coming... {progress}%
+        <p className="text-center text-3xl font-bold mb-6 mt-4 text-[#fff] uppercase">
+          QLV 2024 is Coming...
         </p>
       </div>
     </div>
