0e817dc8bf21fba964709b1f77fb40ddd0626023
Muhammad Najeeb <yusnaj21@gmail.com>
2024-09-19 12:21:05 +0100

    Modified:Loading page

--- src/components/QLV/Loading.jsx
+++ src/components/QLV/Loading.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import useWindowSize from "../../hooks/useWindowSize";
 
-// import QLVLogo from "../QLV/assets/QLV1.png";
-import QLVLogo1 from "../QLV/assets/QLV2.png";
+import QLVBanner from "../QLV/assets/QLV2.png";
+import background from "../QLV/assets/QLVBackground.png";
+import jciLogo from "../QLV/assets/JCI(White).svg";
+import QLV from "../QLV/assets/QLV(White).png";
 
 const LandingPage = () => {
+  const { width } = useWindowSize();
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
@@ -18,18 +22,46 @@ const LandingPage = () => {
     <div
       className="bg-gray-100 bg-cover bg-center flex justify-center items-center min-h-screen"
       style={{
-        backgroundImage: `url(${QLVLogo1})`,
+        backgroundImage: `url(${width < 950 ? background : QLVBanner})`,
+        backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="w-full max-w-md p-6 rounded-lg mt-[30rem]">
-        <div className="w-full bg-gray-300 h-4">
+      <div className="w-full max-w-md p-6 rounded-lg">
+        <div
+          style={{
+            display: `${width < 950 ? "block" : "none"}`,
+            marginBottom: "5rem",
+          }}
+        >
+          <div className="flex justify-center items-center gap-8">
+            <div className="w-[40%]">
+              <img
+                src={jciLogo}
+                alt=""
+                className="w-[100%] object-fit object-cover"
+              />
+            </div>
+
+            <div className="w-[1px] h-[100px] bg-[#fff]"></div>
+
+            <div className="w-[50%]">
+              <img
+                src={QLV}
+                alt=""
+                className="w-[100%] object-fit object-cover"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full bg-gray-500 h-4 lg:mt-[30rem]">
           <div
             className="h-4 striped progress-bar"
             style={{ width: `${progress}%` }}
           ></div>
         </div>
 
-        <p className="text-center text-3xl font-bold mb-6 mt-4 text-[#fff] uppercase">
+        <p className="text-center text-xl md:text-3xl font-semibold mb-6 mt-4 text-[#fff] uppercase">
           QLV 2024 is Coming...
         </p>
       </div>
