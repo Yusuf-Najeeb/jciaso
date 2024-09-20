a0769454cfee9b4a79143553120b508bf50581c1
Muhammad Najeeb <yusnaj21@gmail.com>
2024-09-20 11:07:25 +0100

    Modified:FAQs Section

--- src/components/QLV/FAQs.jsx
+++ src/components/QLV/FAQs.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useState, useEffect, useRef } from "react";
 import { PiCaretUpBold, PiCaretDownBold } from "react-icons/pi";
 
-const Accordion = ({ content }) => {
+const Accordion = ({ data }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [height, setHeight] = useState(0);
   const contentRef = useRef(null);
@@ -27,7 +27,7 @@ const Accordion = ({ content }) => {
           onClick={toggleAccordion}
           className="w-full flex justify-between items-center text-left transition-all duration-200"
         >
-          <span className="font-medium text-2xl">{content.title}</span>
+          <span className="font-medium text-2xl">{data.title}</span>
           {isOpen ? (
             <PiCaretUpBold
               className="p-1 text-xl bg-sky-600 hover:bg-white hover:drop-shadow text-white hover:text-sky-600 rounded-full transition ease-in-out "
@@ -46,9 +46,7 @@ const Accordion = ({ content }) => {
             style={{ height }}
             className="overflow-hidden transition-all duration-500 ease-in-out"
           >
-            <p className="text-xl text-[#333] w-[60%] my-3">
-              {content.content}
-            </p>
+            <p className="text-xl text-[#333] w-[60%] my-3">{data.content}</p>
           </div>
         )}
       </div>
@@ -63,34 +61,29 @@ const data = [
       "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
   },
   {
-    title: "What is QLV 2024 Boot Camp?",
+    title: "Who can attend QLV 2024?",
     content:
-      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+      "The event is open to young professionals, aspiring leaders, JCI Aso members, and non-members interested in leadership and national development.",
   },
   {
-    title: "What is QLV 2024 Boot Camp?",
+    title: "How can I register for the event?",
     content:
-      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+      "You can register for the event by completing the registration form on this page. Ensure all necessary information is provided and select your preferred payment method.",
   },
   {
-    title: "What is QLV 2024 Boot Camp?",
+    title: "What are the dates and formats of the event?",
     content:
-      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+      "QLV 2024 will take place on the 4th, 5th, and 6th of September 2024. The event will be conducted in a hybrid format allowing participants to join online and in-person.",
   },
   {
-    title: "What is QLV 2024 Boot Camp?",
+    title: "What is cost of attending QLV 2024?",
     content:
-      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+      "Registration fees vary depending on the participation format. Please refer to the registration form for detailed pricing information.",
   },
   {
-    title: "What is QLV 2024 Boot Camp?",
+    title: "Will there be networking opportunities?",
     content:
-      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
-  },
-  {
-    title: "What is QLV 2024 Boot Camp?",
-    content:
-      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+      "Yes, QLV 2024 includes numerous opportunities for networking with peers, industry leaders and keynote speakers.",
   },
 ];
 
