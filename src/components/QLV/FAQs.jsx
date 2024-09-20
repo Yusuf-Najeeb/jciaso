6ce0d9c0121df3b05a1f7cf27ae96c33080f5773
Muhammad Najeeb <yusnaj21@gmail.com>
2024-09-20 07:51:17 +0100

    Modified:FAQs Section

--- src/components/QLV/FAQs.jsx
+++ src/components/QLV/FAQs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useState, useEffect, useRef } from "react";
-import { FaCaretDown, FaCaretUp } from "react-icons/fa";
+import { PiCaretUpBold, PiCaretDownBold } from "react-icons/pi";
 
 const Accordion = ({ content }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,17 +21,23 @@ const Accordion = ({ content }) => {
   }, [isOpen]);
 
   return (
-    <div className="w-full mx-auto mt-5">
-      <div className="bg-white p-8 rounded-lg">
+    <div className="w-full mx-auto">
+      <div className="bg-white p-8">
         <button
           onClick={toggleAccordion}
           className="w-full flex justify-between items-center text-left transition-all duration-200"
         >
-          <span className="font-medium">{content.title}</span>
+          <span className="font-medium text-2xl">{content.title}</span>
           {isOpen ? (
-            <FaCaretUp className="text-gray-500" />
+            <PiCaretUpBold
+              className="p-1 text-xl bg-sky-600 hover:bg-white hover:drop-shadow text-white hover:text-sky-600 rounded-full transition ease-in-out "
+              size={40}
+            />
           ) : (
-            <FaCaretDown className="text-gray-500" />
+            <PiCaretDownBold
+              className="p-1 text-xl bg-sky-600 hover:bg-white hover:drop-shadow text-white hover:text-sky-600 rounded-full transition ease-in-out "
+              size={40}
+            />
           )}
         </button>
         {isOpen && (
@@ -40,7 +46,9 @@ const Accordion = ({ content }) => {
             style={{ height }}
             className="overflow-hidden transition-all duration-500 ease-in-out"
           >
-            <p className="text-gray-600">{content.content}</p>
+            <p className="text-xl text-[#333] w-[60%] my-3">
+              {content.content}
+            </p>
           </div>
         )}
       </div>
@@ -50,26 +58,46 @@ const Accordion = ({ content }) => {
 
 const data = [
   {
-    title: "What is the duration of the event?",
+    title: "What is QLV 2024 Boot Camp?",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia facilis repellat eius natus, veritatis porro ratione error voluptatum ullam possimus ut vitae. Libero deserunt temporibus voluptates rerum quos cumque. Magni.",
+      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
   },
   {
-    title: "What is the duration of the event?",
+    title: "What is QLV 2024 Boot Camp?",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia facilis repellat eius natus, veritatis porro ratione error voluptatum ullam possimus ut vitae. Libero deserunt temporibus voluptates rerum quos cumque. Magni.",
+      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
   },
   {
-    title: "What is the duration of the event?",
+    title: "What is QLV 2024 Boot Camp?",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia facilis repellat eius natus, veritatis porro ratione error voluptatum ullam possimus ut vitae. Libero deserunt temporibus voluptates rerum quos cumque. Magni.",
+      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+  },
+  {
+    title: "What is QLV 2024 Boot Camp?",
+    content:
+      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+  },
+  {
+    title: "What is QLV 2024 Boot Camp?",
+    content:
+      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+  },
+  {
+    title: "What is QLV 2024 Boot Camp?",
+    content:
+      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
+  },
+  {
+    title: "What is QLV 2024 Boot Camp?",
+    content:
+      "QLV 2024 Boot Camp is an event designed to help develop leadership skills, promote ethical practices, and inspire young professionals. It includes keynote speeches, interactive workshops, and networking opportunities.",
   },
 ];
 
 const FAQs = () => {
   return (
     <main className="bg-[#FCFCFC] text-[#001926] pt-[6rem] px-[5rem] mx-auto mb-8">
-      <h2 className="text-5xl font-bold text-center">FAQs</h2>
+      <h2 className="text-5xl font-bold text-center mb-4">FAQs</h2>
       <div>
         {data.map((item, i) => {
           return <Accordion key={i} content={item} />;
